c1c7ee37e3b239ba9e4c4756b8df0ee04a20e905
busay.virgi <rahmanbujangga@gmail.com>
2020-09-19 15:25:50 +0700

    Implement pagination

--- src/main/java/com/bujangga/springdemo/repository/UserRepository.java
+++ src/main/java/com/bujangga/springdemo/repository/UserRepository.java
@@ -1,11 +1,12 @@
 package com.bujangga.springdemo.repository;
 
 import com.bujangga.springdemo.io.entity.UserEntity;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends CrudRepository<UserEntity, Long> {
+public interface UserRepository extends PagingAndSortingRepository<UserEntity, Long> {
     UserEntity findUserByEmail(String email);
+
     UserEntity findByUserId(String userId);
 }
--- src/main/java/com/bujangga/springdemo/service/impl/UserServiceImpl.java
+++ src/main/java/com/bujangga/springdemo/service/impl/UserServiceImpl.java
@@ -9,6 +9,9 @@ import com.bujangga.springdemo.shared.utils.UserUtils;
 import com.bujangga.springdemo.ui.model.response.ErrorMessages;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -16,6 +19,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -84,6 +88,23 @@ public class UserServiceImpl implements UserService {
         UserEntity userEntity = userRepository.findByUserId(userId);
         if (userEntity == null) throw new UserServiceException(ErrorMessages.NO_RECORD_FOUND.getErrorMessage());
         userRepository.delete(userEntity);
+    }
+
+    @Override
+    public List<UserDto> getUsers(int page, int limit) {
+        List<UserDto> returnValue = new ArrayList<>();
+        if (page > 0) page = page - 1;
+        Pageable pageableRequest = PageRequest.of(page, limit);
+        Page<UserEntity> usersPage = userRepository.findAll(pageableRequest);
+        List<UserEntity> users = usersPage.getContent();
+
+        for (UserEntity user : users) {
+            UserDto userDto = new UserDto();
+            BeanUtils.copyProperties(user, userDto);
+            returnValue.add(userDto);
+        }
+
+        return returnValue;
     }
 
     @Override
